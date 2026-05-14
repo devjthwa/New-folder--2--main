268f640f5c0b77d7abc373bcefd7af68bbe35516
Dev Jethwa <devjethva1702@gmail.com>
2026-05-14 14:30:40 +0530

    now changed

--- my-react-app/src/ExampleWithDesign.tsx
+++ my-react-app/src/ExampleWithDesign.tsx
@@ -6,7 +6,7 @@ export default function ExampleWithDesign() {
   <img className="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/erin-lindford.jpg" alt="" />
   <div className="space-y-2 text-center sm:text-left">
     <div className="space-y-0.5">
-      <p className="text-lg font-semibold text-black">Dev</p>
+      <p className="text-lg font-semibold text-black">Dev Jethva</p>
       <p className="font-medium text-gray-500">Product Engineer</p>
     </div>
     <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
